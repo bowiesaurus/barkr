ae64a79e42f772f29668a71fc13bf9bdf878ac47
bowiesaurus <carolannjones@gmail.com>
2014-07-16 13:21:23 -0400

    Seeded 'breeds' drop-down in new pet form, added pet photos to populate database with

--- scab_scraper.rb
+++ scab_scraper.rb
@@ -1,2 +1,10 @@
 require 'nokogiri'
+require 'open-uri'
 
+page = Nokogiri::HTML(open('http://www.akc.org/breeds/complete_breed_list.cfm'))
+
+breed_list = page.css('ul.common_ul li a')
+breed_list.each do |link|
+ puts "'#{link.text.strip}',"
+ break if link.text.strip == 'Yorkshire Terrier'
+end
